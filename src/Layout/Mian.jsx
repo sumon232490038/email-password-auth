1b0817f31c2a313b4ef4c1fdda679a6094fbe0d9
sumon232 <sumonmia4526@gmail.com>
2024-11-26 01:12:20 +0600

    tailwindcss and daisyUi install done

---
+++ src/Layout/Mian.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Mian = () => {
+  return (
+    <div>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default Mian;
